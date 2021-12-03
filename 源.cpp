973445276f866b25511a4ac493b1e8937d4a4427
键盘上跳舞 <95458827+givemehair@users.noreply.github.com>
2021-12-03 12:00:03 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void jolly();
+void deny();
+int main()
+{
+	jolly();
+	deny();
+	return 0;
+}
+void jolly()
+{
+	printf("for he's a jolly good fellow!\n");
+	printf("for he's a jolly good fellow!\n");
+	printf("for he's a jolly good fellow!\n");
+}
+void deny()
+{
+	printf("which nobody can deny!\n");
+}
