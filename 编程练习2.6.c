469e15a0bac4dbcf6bce92c6e821ff7392068093
键盘上跳舞 <95458827+givemehair@users.noreply.github.com>
2021-12-03 12:08:35 +0800

    Add files via upload

---
+++ 编程练习2.6.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void)
+{
+    int toes = 10,toes2,toes3;
+    //toes3 = toes ^ 2
+    //“^”为二进制按位或异，还不会这些
+    //printf("%d\n",toes3);输出为8
+    printf("tors=10\ntoes*2=%d\n", toes * 2);
+    printf("toes*toes=%d\n", toes * toes);
+    return 0;
+
+}
